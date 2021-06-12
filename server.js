67791806ea286c4970c70676bc20baca41c3af81
yajatvishwak <yajat472001@gmail.com>
2021-06-12 20:50:35 +0530

    📦 NEW: loadtesting

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(morgan("dev"));
 app.set("trust proxy", 1);
 app.use(cors());
 app.use(express.json());
-app.use("/email", emailRouter);
-app.use("/contact", contactRouter);
+app.use("/email", apiLimiter, emailRouter);
+app.use("/contact", apiLimiter, contactRouter);
 
 app.get("/", (req, res) => {
   res.send("Server online");
