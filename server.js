3a6ec5ed6069ac2a19a3cd888fcd6f3e516f6737
yajatvishwak <yajat472001@gmail.com>
2021-06-12 14:31:30 +0530

    📦 NEW: cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const { contactRouter } = require("./router/contact.router");
 const { emailRouter } = require("./router/email.router");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/email", emailRouter);
 app.use("/contact", contactRouter);
